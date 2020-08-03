e23b7817bf241a1b29bf3412e638c118d4d3b213
wangmiao <wangiao@medicinovo.com>
2020-08-03 10:23:24 +0800

    init juc

---
+++ src/main/java/com/DesignPatterns/Strategy/MyMain.java
@@ -0,0 +1,17 @@
+package com.DesignPatterns.Strategy;
+
+import org.springframework.cglib.util.ParallelSorter;
+
+/**
+ * @Author: hataki
+ * @Date: 2020/8/3
+ * Time: 10:19
+ * description:
+ */
+public class MyMain {
+
+    int[] aa = {5,4,99,2,33,6,49,75,63,56};
+
+
+
+}
---
+++ src/main/java/com/DesignPatterns/Strategy/Sorter.java
@@ -0,0 +1,10 @@
+package com.DesignPatterns.Strategy;
+
+/**
+ * @Author: hataki
+ * @Date: 2020/8/3
+ * Time: 10:22
+ * description:
+ */
+public class Sorter {
+}

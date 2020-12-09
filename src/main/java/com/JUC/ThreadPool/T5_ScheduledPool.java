5bb2965f830fa8fa94c4fd934a5f2c6042e5f45e
wangmiao <918693668@qq.com>
2020-12-09 23:27:26 +0800

    add scheduleThreadPool code

---
+++ src/main/java/com/JUC/ThreadPool/T5_ScheduledPool.java
@@ -0,0 +1,23 @@
+package com.JUC.ThreadPool;
+
+import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * add code
+ */
+public class T5_ScheduledPool {
+    public static void main(String[] args) {
+        ScheduledExecutorService service = Executors.newScheduledThreadPool(4);
+        service.scheduleAtFixedRate( () -> {
+            try {
+                TimeUnit.MILLISECONDS.sleep(new Random().nextInt(1000));
+            }catch(InterruptedException e){
+                e.printStackTrace();
+            }
+            System.out.println( Thread.currentThread().getName());
+        },0,500, TimeUnit.MILLISECONDS);
+    }
+}

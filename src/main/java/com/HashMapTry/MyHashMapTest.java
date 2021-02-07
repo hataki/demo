e0609907434e1d2582f72e44212009fa8dcf5f0d
wangmiao <918693668@qq.com>
2021-02-07 23:47:21 +0800

    cap principle a little update 2

---
+++ src/main/java/com/HashMapTry/MyHashMapTest.java
@@ -0,0 +1,25 @@
+package com.HashMapTry;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runners.JUnit4;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MyHashMapTest {
+
+
+    @Test
+    public void MyHashMapTest(){
+
+
+        Map map = new HashMap(64);
+        for (int i = 0; i < 100000000; i++) {
+            map.put("test","test is ok !");
+            System.err.println( map.get("test") + " :" + i );
+        }
+
+
+    }
+}

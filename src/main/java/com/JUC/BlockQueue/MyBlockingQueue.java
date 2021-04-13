dea9b7b4f057a798c0bd63c25c0d63710f2e5061
wangmiao <918693668@qq.com>
2021-04-14 00:13:04 +0800

    add my blocking queue

---
+++ src/main/java/com/JUC/BlockQueue/MyBlockingQueue.java
@@ -0,0 +1,8 @@
+package com.JUC.BlockQueue;
+
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.DelayQueue;
+
+public class MyBlockingQueue  extends DelayQueue {
+}
